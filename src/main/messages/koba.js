e9875c5a88cad56e886d995fb957434c0948fb79
Murata Nao <n.murata315370@gmail.com>
2026-04-27 15:07:15 +0900

    全員の弾幕実装

--- src/main/messages/koba.js
+++ src/main/messages/koba.js
@@ -1,5 +1,5 @@
-window.murataConfig = {
-    color: #00ffb7, // ここに好きな色（16進数や英語）を書いてもらう
+window.kobaConfig = {
+    color: "#fbff00", // ここに好きな色（16進数や英語）を書いてもらう
     messages: [
         "PROJECT: spaceに到着しました",
         "やったー！"
