cb82572f1178301625b0a3b5106734cdf43038c7
hibiki1234-5-hash <hibiki.1234-5@i.softbank.jp>
2026-04-27 14:20:20 +0900

    nakahara0201

---
+++ src/main/messages/nakahara.js
@@ -0,0 +1,8 @@
+window.nakaharaConfig = {
+    color: "#00ff00",
+    message: [
+        "PROJECT: space‚Ö‚æ‚¤‚±‚»I - by Nakahara",
+        "’e–‹‚ÌÀ‘•‚É¬Œ÷‚µ‚Ü‚µ‚½I",
+        "Š®¬‚Ü‚Å·‚èã‚°‚Ä‚¢‚«‚Ü‚µ‚å‚¤I"
+    ]
+};
