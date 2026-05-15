59d393815efa5425be918d5aab7415052a9d39a1
Murata Nao <n.murata315370@gmail.com>
2026-05-15 12:54:33 +0900

    壁紙化対応中

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// 作成した各ページをインポート  修正中
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Wallpaper from './pages/Wallpaper/Wallpaper';
@@ -12,20 +10,11 @@ function App() {
     return (
         <Router>
             <Routes>
-                {/* http://localhost:5173/ の時 */}
                 <Route path="/" element={<Home />} />
-
-                {/* http://localhost:5173/wallpaper の時 */}
                 <Route path="/wallpaper" element={<Wallpaper />} />
-
-                {/* http://localhost:5173/settings の時 */}
                 <Route path="/settings" element={<Settings />} />
-
-                {/* http://localhost:5173/collection の時 */}
                 <Route path="/collection" element={<Collection />} />
-
-                {/* 存在しないURLが打たれた時はHomeへ */}
-                <Route path="*" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
     );
