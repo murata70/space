c55bf5309f2644493f76ea4eae7362117df3dcaf
Murata Nao <n.murata315370@gmail.com>
2026-05-19 15:29:29 +0900

    近未来カー修正

--- src/components/ocean_ui/flows/FuturisticCarFlow/FuturisticCarFlow.jsx
+++ src/components/ocean_ui/flows/FuturisticCarFlow/FuturisticCarFlow.jsx
@@ -4,21 +4,24 @@ import "./FuturisticCarFlow.css";
 const publicUrl = process.env.PUBLIC_URL || "";
 
 export default function FuturisticCarFlow({ onComplete }) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      onComplete?.();
-    }, 9000);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onComplete?.();
+        }, 9000);
 
-    return () => clearTimeout(timer);
-  }, [onComplete]);
+        return () => clearTimeout(timer);
+    }, [onComplete]);
 
-  return (
-    <div className="futuristic-car-flow">
-      <img
-        src={`${publicUrl}/assets/ocean_image/collections/futuristic_car.png`}
-        alt="futuristic car"
-        className="futuristic-car-image"
-      />
-    </div>
-  );
+    return (
+        <div className="futuristic-car-flow">
+            {/* imgへのアニメーション上書き強制を回避するため、外枠を一枚挟みます */}
+            <div className="futuristic-car-body">
+                <img
+                    src={`${publicUrl}/assets/ocean_image/collections/futuristic_car.png`}
+                    alt="futuristic car"
+                    className="futuristic-car-image"
+                />
+            </div>
+        </div>
+    );
 }
