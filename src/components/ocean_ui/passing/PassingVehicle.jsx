45ec591a0cb8bbef5d29afbbdf1dda19852ec71b
gen2000323 <gen2000323@gmail.com>
2026-05-20 15:17:50 +0900

    奥邨

--- src/components/ocean_ui/passing/PassingVehicle.jsx
+++ src/components/ocean_ui/passing/PassingVehicle.jsx
@@ -3,28 +3,25 @@ import "./PassingVehicle.css";
 
 const PASS_DURATION_MS = 12000;
 
-export default function PassingVehicle({
-  image,
-  onComplete,
-}) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      onComplete?.();
-    }, PASS_DURATION_MS);
+export default function PassingVehicle({ image, onComplete }) {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (onComplete) {
+                onComplete();
+            }
+        }, PASS_DURATION_MS);
 
-    return () => clearTimeout(timer);
-  }, [onComplete]);
+        return () => clearTimeout(timer);
+    }, [onComplete]);
 
-  if (!image) return null;
+    if (!image) return null;
 
-  return (
-    <div className="passing-wrapper">
-      <img
-        src={image}
-        className="passing-car"
-        alt="passing car"
-        draggable="false"
-      />
-    </div>
-  );
+    return (
+        <img
+            src={image}
+            className="passing-car"
+            alt="passing car"
+            draggable="false"
+        />
+    );
 }
